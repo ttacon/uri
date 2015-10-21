9083e45754d8dcde1484044822d6342ecbd4a730
ttacon <ttacon@gmail.com>
2015-10-20 22:05:00 -0400

    Add authority info parsing

--- uri.go
+++ uri.go
@@ -39,28 +39,35 @@ func ParseURI(raw string) (URI, error) {
 	hierPartEnd := strings.Index(raw, question)
 	if hierPartEnd < 0 || hierPartEnd == len(raw) {
 		return &uri{
-			scheme:   scheme,
-			hierPart: raw, // since it's all that's left
+			scheme:    scheme,
+			hierPart:  raw, // since it's all that's left
+			authority: parseAuthority(raw),
 		}, nil
 	}
 
 	hierPart := raw[:hierPartEnd]
+
+	// parse authority
+	authorityInfo := parseAuthority(hierPart)
+
 	raw = raw[hierPartEnd+1:]
 
 	queryEnd := strings.Index(raw, fragment)
 	if queryEnd < 0 || queryEnd == len(raw) {
 		return &uri{
-			scheme:   scheme,
-			hierPart: hierPart,
-			query:    raw,
+			scheme:    scheme,
+			hierPart:  hierPart,
+			query:     raw,
+			authority: authorityInfo,
 		}, nil
 	}
 
 	return &uri{
-		scheme:   scheme,
-		hierPart: hierPart,
-		query:    raw[:queryEnd],
-		fragment: raw[queryEnd+1:],
+		scheme:    scheme,
+		hierPart:  hierPart,
+		query:     raw[:queryEnd],
+		fragment:  raw[queryEnd+1:],
+		authority: authorityInfo,
 	}, nil
 }
 
@@ -70,6 +77,9 @@ type uri struct {
 	hierPart string
 	query    string
 	fragment string
+
+	// parsed components
+	authority authorityInfo
 }
 
 func (u *uri) Scheme() string {
@@ -93,3 +103,51 @@ var (
 	ErrNoSchemeFound = errors.New("no scheme found in URI")
 	ErrInvalidURI    = errors.New("not a valid URI")
 )
+
+type authorityInfo struct {
+	userinfo string
+	host     string
+	port     string
+	path     string
+}
+
+func parseAuthority(hier string) authorityInfo {
+	// as per RFC 3986 Section 3.6
+	var userinfo, host, port, path string
+
+	// authority sections can begin with a '//'
+	hier = strings.TrimPrefix(hier, "//")
+
+	slashEnd := strings.Index(hier, "/")
+	if slashEnd > 0 {
+		if slashEnd+1 < len(hier) {
+			path = hier[slashEnd:]
+		}
+		hier = hier[:slashEnd]
+	}
+
+	// authority   = [ userinfo "@" ] host [ ":" port ]
+	host = hier
+	if at := strings.Index(host, "@"); at > 0 {
+		userinfo = host[:at]
+
+		if at+1 < len(host) {
+			host = host[at+1:]
+		}
+	}
+
+	if colon := strings.Index(host, ":"); colon > 0 {
+		if colon+1 < len(host) {
+			port = host[colon+1:]
+		}
+
+		host = host[:colon]
+	}
+
+	return authorityInfo{
+		userinfo: userinfo,
+		host:     host,
+		port:     port,
+		path:     path,
+	}
+}
--- uri_test.go
+++ uri_test.go
@@ -13,11 +13,49 @@ func Test_rawURIParse(t *testing.T) {
 	}{
 		{
 			"foo://example.com:8042/over/there?name=ferret#nose",
-			&uri{
-				"foo",
-				"//example.com:8042/over/there",
-				"name=ferret",
-				"nose",
+			&uri{"foo", "//example.com:8042/over/there", "name=ferret", "nose",
+				authorityInfo{
+					"",
+					"example.com",
+					"8042",
+					"/over/there",
+				},
+			},
+			nil,
+		},
+		{
+			"http://httpbin.org/get?utf8=\xe2\x98\x83",
+			&uri{"http", "//httpbin.org/get", "utf8=\xe2\x98\x83", "",
+				authorityInfo{
+					"",
+					"httpbin.org",
+					"",
+					"/get",
+				},
+			},
+			nil,
+		},
+		{
+			"mailto:user@domain.com",
+			&uri{"mailto", "user@domain.com", "", "",
+				authorityInfo{
+					"user",
+					"domain.com",
+					"",
+					"",
+				},
+			},
+			nil,
+		},
+		{
+			"ssh://user@git.openstack.org:29418/openstack/keystone.git",
+			&uri{"ssh", "//user@git.openstack.org:29418/openstack/keystone.git", "", "",
+				authorityInfo{
+					"user",
+					"git.openstack.org",
+					"29418",
+					"/openstack/keystone.git",
+				},
 			},
 			nil,
 		},
